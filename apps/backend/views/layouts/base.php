3c9008b3b597d79ec0e64a948fb64d7e51482dce
HUYTA08 <tahuygl@gmail.com>
2019-08-10 08:27:37 +0700

    no message

--- apps/backend/views/layouts/base.php
+++ apps/backend/views/layouts/base.php
@@ -1,4 +1,5 @@
 <?php
+
 use backend\assets\BackendAsset;
 use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
@@ -14,29 +15,28 @@ $this->params['body-class'] = array_key_exists('body-class', $this->params) ?
 ?>
 
 <?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?php echo Yii::$app->language ?>">
-<head>
-    <meta charset="<?php echo Yii::$app->charset ?>">
-    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!DOCTYPE html>
+    <html lang="<?php echo Yii::$app->language ?>">
+    <head>
+        <meta charset="<?php echo Yii::$app->charset ?>">
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <?php echo Html::csrfMetaTags() ?>
+        <title><?php echo Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
 
-    <?php echo Html::csrfMetaTags() ?>
-    <title><?php echo Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-
-</head>
-<?php echo Html::beginTag('body', [
-    'class' => implode(' ', [
-        ArrayHelper::getValue($this->params, 'body-class'),
-        Yii::$app->keyStorage->get('backend.theme-skin', 'skin-blue'),
-        Yii::$app->keyStorage->get('backend.layout-fixed') ? 'fixed' : null,
-        Yii::$app->keyStorage->get('backend.layout-boxed') ? 'layout-boxed' : null,
-        Yii::$app->keyStorage->get('backend.layout-collapsed-sidebar') ? 'sidebar-collapse' : null,
-    ])
-])?>
+    </head>
+    <?php echo Html::beginTag('body', [
+        'class' => implode(' ', [
+            ArrayHelper::getValue($this->params, 'body-class'),
+            Yii::$app->keyStorage->get('backend.theme-skin', 'skin-blue'),
+            Yii::$app->keyStorage->get('backend.layout-fixed') ? 'fixed' : null,
+            Yii::$app->keyStorage->get('backend.layout-boxed') ? 'layout-boxed' : null,
+            Yii::$app->keyStorage->get('backend.layout-collapsed-sidebar') ? 'sidebar-collapse' : null,
+        ])
+    ]) ?>
     <?php $this->beginBody() ?>
-        <?php echo $content ?>
+    <?php echo $content ?>
     <?php $this->endBody() ?>
-<?php echo Html::endTag('body') ?>
-</html>
+    <?php echo Html::endTag('body') ?>
+    </html>
 <?php $this->endPage() ?>
